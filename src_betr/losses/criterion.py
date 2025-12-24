14e7ad82e69f470ca8f2d76334fd1a6f1fb4eaf1
Jeon <cjwww826@ucla.edu>
2025-12-24 11:57:53 +0900

    12/24

--- src_betr/losses/criterion.py
+++ src_betr/losses/criterion.py
@@ -36,12 +36,23 @@ class BETRLoss(nn.Module):
         loss_center = loss_coarse + self.lambda_fine * loss_fine
 
         # -- [Weighted Dense Losses] --
-        gt_offset_map = batch['gt_offsets_3d'].view(-1, 16, 1, 1).expand(-1, -1, self.heatmap_size, self.heatmap_size)  # [B, 16, 128, 128]
-        loss_offset = self._weighted_loss(preds['bb8 offsets'], gt_offset_map, weight_map)
+        gt_offsets_map = batch['gt_offsets_3d'].view(-1, 16, 1, 1).expand(-1, -1, self.heatmap_size, self.heatmap_size)  # [B, 16, 128, 128]
+        loss_offset = self._weighted_loss(preds['bb8 offsets'], gt_offsets_map, weight_map)
 
         gt_depth_map = batch['gt_center_depth'].view(-1, 1, 1, 1).expand(-1, 1, self.heatmap_size, self.heatmap_size)  # [B, 1, 128, 128]
         loss_depth = self._weighted_masked_loss(preds['center depth'], gt_depth_map, weight_map)
 
+        gt_depth_offsets_map = batch['gt_depth_offsets'].view(-1, 8, 1, 1).expand(-1, -1, self.heatmap_size, self.heatmap_size)  # [B, 8, 128, 128]
+        loss_depth_offset = self._weighted_masked_loss(preds['bb8 depth offsets'], gt_depth_offsets_map, weight_map)
+
+        total_loss = loss_center + loss_offset + loss_depth + loss_depth_offset
+        return {
+            "total_loss": total_loss,
+            "loss_center": loss_center,
+            "loss_offset": loss_offset,
+            "loss_depth": loss_depth,
+            "loss_depth_offset": loss_depth_offset,
+        }
     def _weighted_loss(self, pred, target, weight_map):
         loss = self.smooth_l1(pred, target)
         weighted_loss = (loss * weight_map).sum() / (weight_map.sum() * pred.shape[1]+ 1e-8)
